77e1215653d28ce38eb4acac6f2b0975b970ebda
RitvijSrivastava <ritvijsrivastava99@gmail.com>
2021-03-01 14:38:40 +0530

    :sparkles: Add MongoDB Atlas

--- app.js
+++ app.js
@@ -4,14 +4,32 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const session = require("express-session");
+const connectDB = require("./config/connect_db");
+const { default: MongoStore } = require("connect-mongo");
 
 // Init Express
 const app = express();
 
+// Fetch ENV variables
 dotenv.config({ path: "./.env" });
+
+// Setup Sessions
+// !! SECRET Always required. Not having it in '.env' will leave the app unsecure.
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+  })
+);
 
 app.use(cors());
 app.use(bodyParser.json());
+
+// Connect Database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
---
+++ config/connect_db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "../.env" });
+
+const connectDB = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    });
+
+    console.log(`MongoDB Connected: ${connection.connection.host}`);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
