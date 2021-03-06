439693aff0fca911292dd716ce4597043f9e02ff
RitvijSrivastava <ritvijsrivastava99@gmail.com>
2021-03-06 22:43:03 +0530

    :card_file_box: Add basic routes for user

---
+++ controllers/user.js
@@ -0,0 +1,34 @@
+const User = require("../models/User");
+
+exports.getUserById = (req, res) => {
+  const userId = req.query.userId;
+  User.findById(userId)
+    .lean()
+    .populate("teams")
+    .exec((err, user) => {
+      if (err) {
+        return res
+          .status(400)
+          .json({ error: "Error occured! Cannot fetch user!" });
+      } else {
+        delete user.encrypted_password;
+        delete user.salt;
+        delete user.updatedAt;
+        return res.status(200).json(user);
+      }
+    });
+};
+
+exports.updateUserProfile = (req, res) => {
+  const userId = req.query.userId;
+  const userData = req.body.data;
+  User.findByIdAndUpdate(userId, { ...userData }, function (err, docs) {
+    if (err) {
+      return res.status(400).json({
+        error: "Some error occured! Cannot update profile at this time!",
+      });
+    } else {
+      return res.status(200).json({ message: "Profile updated successfully!" });
+    }
+  });
+};
