e3a31bd17f8fd5d73eb517da96457e862a455d48
RitvijSrivastava <ritvijsrivastava99@gmail.com>
2021-05-09 01:28:59 +0530

    Change cors origin

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ const PORT = process.env.PORT || 5000;
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "https://collabb.netlify.app/",
+    origin: "*",
     methods: ["GET", "POST", "PUT"],
     credentials: true,
   },
